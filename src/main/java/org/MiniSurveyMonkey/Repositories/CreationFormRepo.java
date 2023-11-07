cc0895b9e02ece13c2cba3b84735982d9ae6bae1
andre-Hazim <andre.hazim1@gmail.com>
2023-11-06 19:45:01 -0500

    added CreationFormRepo

---
+++ src/main/java/org/MiniSurveyMonkey/Repositories/CreationFormRepo.java
@@ -0,0 +1,7 @@
+package org.MiniSurveyMonkey.Repositories;
+import org.MiniSurveyMonkey.Forms.CreationForm;
+import org.springframework.data.mongodb.repository.MongoRepository;
+public interface CreationFormRepo extends MongoRepository<CreationForm, String>{
+
+
+}
