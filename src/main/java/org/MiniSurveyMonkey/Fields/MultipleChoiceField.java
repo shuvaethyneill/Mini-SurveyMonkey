20da3098e48286e34372616ca275cee73ecd6c6e
shuvaethyneill <shuvaethy.neill@gmail.com>
2023-11-08 17:47:06 -0500

    Added MC field object

--- src/main/java/org/MiniSurveyMonkey/Fields/MultipleChoiceField.java
+++ src/main/java/org/MiniSurveyMonkey/Fields/MultipleChoiceField.java
@@ -1,12 +1,52 @@
 package org.MiniSurveyMonkey.Fields;
 
 import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.HashMap;
 
 @Document("mcField")
-public class MultipleChoiceField extends Field{
+public class MultipleChoiceField extends Field {
+    private HashMap<String, String> options;
+    private String selectedOption;
 
-
-    public MultipleChoiceField(String question) {
+    /**
+     * Constructor for MC field
+     * @param question, the question associated with this field
+     * @param options, the hashmap of option name and value
+     */
+    public MultipleChoiceField(String question, HashMap<String, String> options) {
         super(question, FieldType.MC);
+        this.options = options;
+    }
+
+    /**
+     * Method to get options
+     * @return HashMap<String, String> options
+     */
+    public HashMap<String, String> getOptions() {
+        return options;
+    }
+
+    /**
+     * Method to set options
+     * @param options
+     */
+    public void setOptions(HashMap<String, String> options) {
+        this.options = options;
+    }
+
+    /**
+     * Method to get the selected option
+     * @return String value of selected option
+     */
+    public String getSelectedOption() {
+        return selectedOption;
+    }
+
+    /**
+     * Method to set the selected option
+     * @param selectedOption
+     */
+    public void setSelectedOption(String selectedOption) {
+        this.selectedOption = selectedOption;
     }
 }
