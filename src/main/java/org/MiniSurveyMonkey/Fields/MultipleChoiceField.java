69ea674020fb8c1b51e4658c4e9257912638e374
shuvaethyneill <shuvaethy.neill@gmail.com>
2023-11-09 11:44:39 -0500

    Changed HashMap to ArrayList

--- src/main/java/org/MiniSurveyMonkey/Fields/MultipleChoiceField.java
+++ src/main/java/org/MiniSurveyMonkey/Fields/MultipleChoiceField.java
@@ -1,28 +1,29 @@
 package org.MiniSurveyMonkey.Fields;
 
 import org.springframework.data.mongodb.core.mapping.Document;
-import java.util.HashMap;
+
+import java.util.ArrayList;
 
 @Document("mcField")
 public class MultipleChoiceField extends Field {
-    private HashMap<String, String> options;
+    private ArrayList<String> options;
     private String selectedOption;
 
     /**
      * Constructor for MC field
      * @param question, the question associated with this field
-     * @param options, the hashmap of option name and value
+     * @param options, the list of options
      */
-    public MultipleChoiceField(String question, String formId, HashMap<String, String> options) {
+    public MultipleChoiceField(String question, String formId, ArrayList<String> options) {
         super(question, FieldType.MC, formId);
         this.options = options;
     }
 
     /**
      * Method to get options
-     * @return HashMap<String, String> options
+     * @return ArrayList<String> options
      */
-    public HashMap<String, String> getOptions() {
+    public ArrayList<String> getOptions() {
         return options;
     }
 
@@ -30,7 +31,7 @@ public class MultipleChoiceField extends Field {
      * Method to set options
      * @param options
      */
-    public void setOptions(HashMap<String, String> options) {
+    public void setOptions(ArrayList<String> options) {
         this.options = options;
     }
 
