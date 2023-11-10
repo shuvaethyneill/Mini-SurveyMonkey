67d36f9a5483b99d870eda539290e6b816114654
shuvaethyneill <shuvaethy.neill@gmail.com>
2023-11-09 20:42:43 -0500

    Inital changes to have dynamic form

--- src/main/resources/public/Form.js
+++ src/main/resources/public/Form.js
@@ -3,42 +3,100 @@
  */
 
 $(document).ready(function () {
-    $("#dropdown").change(function () {
-        var selectedOption = $(this).val();
-        var form = $("#myForm");
-        var dynamicInput = $("#dynamicInput");
+    let questionCount = 1; // first question added by default
 
-        // Remove any existing dynamic input elements
-        dynamicInput.empty();
+    $('#addQuestion').click(function () {
+        questionCount++;
 
-        if (selectedOption === "NumberField" || selectedOption === "TextField") {
-            $("#mcOptionsContainer").hide(); //to ensure that second dropdown is hidden
-            var inputField = $("<input>").attr({
-                type: "text",
-                name: "dynamicInput",
-                placeholder: "User answer here"
-            });
-            dynamicInput.append(inputField);
-        } else if (selectedOption === "MC") {
-            $("#mcOptionsContainer").show();
+        // each question has a div
+        const questionDiv = $('<div>').addClass('question').attr('id', `question${questionCount}`);
 
-            $("#mcOptionsDropdown").change(function () {
-                var selectedOption = $(this).val();
-                dynamicInput.empty();
-                for (var i = 1; i <= selectedOption; i++) {
-                    var radioBtn = $("<input>").attr({
-                        type: "radio",
-                        name: "dynamicInput"
-                    });
-                    var labelInput = $("<input>").attr({
-                        type: "text",
-                        placeholder: "Enter mc option text"
-                    });
-                    dynamicInput.append(radioBtn).append(labelInput).append("<br>");
-                }
-            })
+        const questionLabel = $('<label>').attr('for', `questionTitle${questionCount}`).text(`Question ${questionCount}`);
+        const questionInput = $('<input>').attr({
+            type: 'text',
+            id: `questionTitle${questionCount}`,
+            name: `questionTitle${questionCount}`,
+            placeholder: 'Enter Survey Question'
+        });
 
-            }
-        })
+        const fieldTypeElements= createFieldTypeElement(questionCount)
+
+        questionDiv.append(questionLabel).append(questionInput).append('<br><br>').append(fieldTypeElements.label).append(fieldTypeElements.dropdown);
+        $('#surveyForm').append(questionDiv);
     });
 
+    $('#surveyForm').on('change', '[id^=fieldType]', function () {
+        const selectedOption = $(this).val();
+        const questionNumber = $(this).attr('id').match(/\d+/)[0];
+        const parentDiv = $(this).closest('.question');
+        const inputContainer = parentDiv.find('.inputContainer');
+
+        if (selectedOption === 'number') {
+            // TODO: show number field
+        } else if (selectedOption === 'text') {
+            // TODO: show text field
+        } else if (selectedOption === 'multipleChoice') {
+            const mcContainer = $('<div>'); // container for multiple choice options
+
+            // initial two multiple choice options
+            mcContainer.append(createMCOption(questionNumber, 1)).append(createMCOption(questionNumber, 2));
+            inputContainer.append(mcContainer);
+
+            // to add more choices
+            const addChoiceBtn = $('<button>').text('Add Choice').click(function () {
+                const optionCount = mcContainer.find('.mcOption').length + 1;
+                mcContainer.append(createMCOption(optionCount));
+                updateRemoveBtns(); // update remove buttons after addition
+            });
+            inputContainer.append(addChoiceBtn);
+        }
+    });
+
+    function createFieldTypeElement(questionCount) {
+        const fieldTypeLabel = $('<label>').attr('for', `fieldType${questionCount}`).text('Choose Field Type:');
+        const fieldTypeDropdown = $('<select>').attr({
+            id: `fieldType${questionCount}`,
+            name: `fieldType${questionCount}`
+        }).html(`
+        <option value="text">Text Field</option>
+        <option value="number">Number Field</option>
+        <option value="multipleChoice">Multiple Choice</option>
+    `);
+
+        return {
+            label: fieldTypeLabel,
+            dropdown: fieldTypeDropdown
+        };
+    }
+
+    // Function to create a multiple choice option
+    function createMCOption(questionNumber, optionCount) {
+        const mcOptionDiv = $('<div>').addClass('mcOption');
+        const radioBtn = $('<input>').attr({
+            type: 'radio',
+            name: `mcOptions${questionNumber}`,
+        });
+
+        const optionInput = $('<input>').attr({
+            type: 'text',
+            name: `mcOption${questionNumber}_${optionCount}`,
+            placeholder: 'Enter Choice'
+        });
+
+        const removeBtn = $('<button>').text('Remove Choice').prop('disabled', true).click(function () {
+            $(this).closest('.mcOption').remove();
+            updateRemoveBtns(); // update remove buttons after removal
+        });
+
+        mcOptionDiv.append(radioBtn, optionInput, removeBtn);
+        return mcOptionDiv;
+    }
+
+    // Function to enable/disable remove buttons for MC options
+    function updateRemoveBtns() {
+        $('.mcOption').each(function (index) {
+            const removeBtn = $(this).find('button');
+            removeBtn.prop('disabled', index < 2);
+        });
+    }
+})
