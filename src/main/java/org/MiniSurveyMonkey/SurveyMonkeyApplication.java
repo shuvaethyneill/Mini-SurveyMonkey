9ce5f1c78935a52e32d6ec1c20436c46848c1275
andre-Hazim <andre.hazim1@gmail.com>
2023-11-11 14:29:51 -0500

    allowed server database connection

--- src/main/java/org/MiniSurveyMonkey/SurveyMonkeyApplication.java
+++ src/main/java/org/MiniSurveyMonkey/SurveyMonkeyApplication.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class SurveyMonkeyApplication implements CommandLineRunner {
 
 	@Autowired
