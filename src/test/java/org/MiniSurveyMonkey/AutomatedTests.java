0abb53ae31fbd702a584f70f9f6c4387b2af5874
guymorgenshtern <33492457+guymorgenshtern@users.noreply.github.com>
2023-11-12 22:06:24 -0500

    testing maven.yml for github actions fix

--- src/test/java/org/MiniSurveyMonkey/AutomatedTests.java
+++ src/test/java/org/MiniSurveyMonkey/AutomatedTests.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -26,7 +27,8 @@ import java.util.Optional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class automatedTests {
+@ContextConfiguration(classes = SurveyMonkeyApplication.class)
+public class AutomatedTests {
     private String formId;
     @Autowired
     private RestController control;
