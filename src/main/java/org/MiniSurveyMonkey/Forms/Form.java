2914fd4bb674a256bb205fda924852179db8e5b9
shuvaethyneill <shuvaethy.neill@gmail.com>
2023-11-26 22:57:56 -0500

    Code cleanup for consistency and javadocs

--- src/main/java/org/MiniSurveyMonkey/Forms/Form.java
+++ src/main/java/org/MiniSurveyMonkey/Forms/Form.java
@@ -8,6 +8,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 
+/**
+ * Class to represent a Form
+ */
 @Document(collection = "Form")
 public class Form {
 
@@ -27,10 +30,18 @@ public class Form {
     private ArrayList<Visualization> visualizations;
 
 
+    /**
+     * Default constructor
+     */
     public Form(){
         this("","");
     }
 
+    /**
+     * Coonstructor for Form
+     * @param formName , name of form
+     * @param author , author of form
+     */
     public Form(String formName, String author) {
         this.formName = formName;
         this.author = author;
@@ -40,77 +51,158 @@ public class Form {
         this.visualizations = new ArrayList<>();
     }
 
+    /**
+     * Method to add a field to the fields list
+     * @param field
+     */
     public void addField(Field field){
         this.fields.add(field);
     }
+
+    /**
+     * Method to add a response to the responses list
+     * @param response
+     */
     public void addResponse(Response response){
         this.responses.add(response);
     }
 
+    /**
+     * Method to remove a field
+     * @param field
+     */
     public void removeField(Field field){this.fields.remove(field);}
 
+    /**
+     * Method to remove a response
+     * @param response
+     */
     public void removeResponse(Response response){this.fields.remove(response);}
 
+    /**
+     * Method to get the id of the form
+     * @return
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Method to set the id of the form
+     * @param id
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Method to get the fields in the form
+     * @return
+     */
     public ArrayList<Field> getFields() {
         return fields;
     }
 
+    /**
+     * Method to set the fields in the form
+     * @param fields
+     */
     public void setFields(ArrayList<Field> fields) {
         this.fields = fields;
     }
 
+    /**
+     * Method to get the form responses
+     * @return
+     */
     public ArrayList<Response> getResponses() {
         return responses;
     }
 
+    /**
+     * Method to set the responses of the form
+     * @param responses
+     */
     public void setResponses(ArrayList<Response> responses) {
         this.responses = responses;
     }
 
+    /**
+     * Method to get the form name (title)
+     * @return
+     */
     public String getFormName() {
         return formName;
     }
 
+    /**
+     * Method to set the form name (title)
+     * @param formName
+     */
     public void setFormName(String formName) {
         this.formName = formName;
     }
 
+    /**
+     * Method to get the author of the form
+     * @return
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Method to set the author of the form
+     * @param author
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    /**
+     * Method to check if the form is closed
+     * @return
+     */
     public boolean isClosed() {
         return closed;
     }
 
+    /**
+     * Method to set the form to be closed
+     * @param closed
+     */
     public void setClosed(boolean closed) {
         this.closed = closed;
     }
 
+    /**
+     * Method to get the list of visualizations
+     * @return
+     */
     public ArrayList<Visualization> getGraphs() {
         return visualizations;
     }
 
+    /**
+     * Method to add a visualization
+     * @param visualization
+     */
     public void addVisualization(Visualization visualization) {
         this.visualizations.add(visualization);
     }
 
+    /**
+     * Method to set the list of visualizations
+     * @param visualizations
+     */
     public void setGraphs(ArrayList<Visualization> visualizations) {
         this.visualizations = visualizations;
     }
 
+    /**
+     * Overriden toString method to represent the form object as a String
+     * @return
+     */
     @Override
     public String toString() {
         return "Form{" +
@@ -122,6 +214,4 @@ public class Form {
                 ", closed=" + closed +
                 '}';
     }
-
-
 }
--- src/main/java/org/MiniSurveyMonkey/Graphs/Visualization.java
+++ src/main/java/org/MiniSurveyMonkey/Graphs/Visualization.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
         @JsonSubTypes.Type(value = Table.class, name = "Table"),
 })
 
-
 public abstract class Visualization {
 
     private String formId;
@@ -20,6 +19,13 @@ public abstract class Visualization {
 
     private VisualizationType visualizationType;
 
+    /**
+     * Constructor for Visualization
+     * @param formId id of the form
+     * @param fieldName name of the field
+     * @param fieldId id of the field
+     * @param visualizationType the type of visualization
+     */
     public Visualization(String formId, String fieldName, String fieldId, VisualizationType visualizationType) {
         this.formId = formId;
         this.fieldName = fieldName;
@@ -27,34 +33,66 @@ public abstract class Visualization {
         this.visualizationType = visualizationType;
     }
 
+    /**
+     * Method to get the id of the form
+     * @return String id
+     */
     public String getFormId() {
         return formId;
     }
 
+    /**
+     * Method to set the id of the form
+     * @param formId
+     */
     public void setFormId(String formId) {
         this.formId = formId;
     }
 
+    /**
+     * Method to get the field name
+     * @return String fieldName
+     */
     public String getFieldName() {
         return fieldName;
     }
 
+    /**
+     * Method to set the field name
+     * @param fieldName
+     */
     public void setFieldName(String fieldName) {
         this.fieldName = fieldName;
     }
 
+    /**
+     * Method to get the id of the field
+     * @return String fieldId
+     */
     public String getFieldId() {
         return fieldId;
     }
 
+    /**
+     * Method to set the id of the field
+     * @param fieldId
+     */
     public void setFieldId(String fieldId) {
         this.fieldId = fieldId;
     }
 
+    /**
+     * Method to get the visualization type
+     * @return
+     */
     public VisualizationType getGraphType(){
         return this.visualizationType;
     }
 
+    /**
+     * Method to set the visualization type
+     * @param visualizationType
+     */
     public void setGraphType(VisualizationType visualizationType){
         this.visualizationType = visualizationType;
     }
